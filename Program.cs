44ca25555d436c359d3002960d4111fbeca04668
Fazrin <nirzaf@gmail.com>
2021-08-12 15:41:19 +0530

    Natural numbers and their sums

--- Program.cs
+++ Program.cs
@@ -6,44 +6,21 @@ namespace app2
     {
         static void Main(string[] args)
         {
-            //    int sum = 0;
-            //     Console.Write("Input ypur Natural Number : "); 
-            //     int input = Convert.ToInt32(Console.ReadLine());
-            //     Console.Write("The first {0} Natural Numbers are : " , input);
-            //     for (int x = 1; x <= input; x++)
-            //    {             
-            //          Console.Write(x + " ");
-                    
-            //          sum += x;
-            //    } 
-            //     Console.WriteLine("\nThe sum is : {0}" , sum);  
-
-        //    int multiple;
-        //    Console.Write("Input Your Number : ");
-        //    int input = Convert.ToInt32(Console.ReadLine());
-        //    for(int x = 1; x <= 10; x++)
-        //    {
-        //       multiple = x * input;
-        //        Console.WriteLine("{0} * {1} = {2}", x , input , multiple ); 
-        //    }
-            Console.WriteLine("Enter Your Input");
+            int sum = 0;
+            Console.Write("input ypur natural number : ");
             int input = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("\n All primae numbers within your input value \n");
-            for (int x = 2; x < input; x++)
+            Console.Write("the first {0} natural numbers are : ", input);
+            for (int x = 1; x <= input; x++)
             {
-         
-                int isPrime = 0;
-                for (int y = 1; y < x; y++)
-                {
-                    if (x % y == 0)
-                        isPrime++;
+                Console.Write(x + " ");
 
-                    if(isPrime == 2) break;
-                }
-                if(isPrime != 2)
-                Console.WriteLine(x);
+                sum += x;
             }
-            Console.ReadKey();
-        }  
+            Console.WriteLine("\nthe sum is : {0}", sum);
+
+
+
+
+        }
     }
 }
