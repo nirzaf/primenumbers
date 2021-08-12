247c04a5c97fe61c3d57b74e0dc3917f329a3f6b
Fazrin <nirzaf@gmail.com>
2021-08-12 15:42:29 +0530

    Multiplication

--- Program.cs
+++ Program.cs
@@ -6,44 +6,14 @@ namespace app2
     {
         static void Main(string[] args)
         {
-            //    int sum = 0;
-            //     Console.Write("Input ypur Natural Number : "); 
-            //     int input = Convert.ToInt32(Console.ReadLine());
-            //     Console.Write("The first {0} Natural Numbers are : " , input);
-            //     for (int x = 1; x <= input; x++)
-            //    {             
-            //          Console.Write(x + " ");
-                    
-            //          sum += x;
-            //    } 
-            //     Console.WriteLine("\nThe sum is : {0}" , sum);  
-
-        //    int multiple;
-        //    Console.Write("Input Your Number : ");
-        //    int input = Convert.ToInt32(Console.ReadLine());
-        //    for(int x = 1; x <= 10; x++)
-        //    {
-        //       multiple = x * input;
-        //        Console.WriteLine("{0} * {1} = {2}", x , input , multiple ); 
-        //    }
-            Console.WriteLine("Enter Your Input");
-            int input = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("\n All primae numbers within your input value \n");
-            for (int x = 2; x < input; x++)
+            int multiple;
+            Console.Write("input your number : ");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            for (int x = 1; x <= 10; x++)
             {
-         
-                int isPrime = 0;
-                for (int y = 1; y < x; y++)
-                {
-                    if (x % y == 0)
-                        isPrime++;
-
-                    if(isPrime == 2) break;
-                }
-                if(isPrime != 2)
-                Console.WriteLine(x);
+                multiple = x * input1;
+                Console.WriteLine("{0} * {1} = {2}", x, input, multiple);
             }
-            Console.ReadKey();
         }  
     }
 }
