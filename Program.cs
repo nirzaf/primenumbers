724785d7f2ba59f68fd6263cf2d43e5fbebffd78
Fazrin <nirzaf@gmail.com>
2021-08-12 22:53:48 +0530

    PrintNumbers

--- Program.cs
+++ Program.cs
@@ -6,15 +6,28 @@ namespace app2
     {
         static void Main(string[] args)
         {
-            int multiple;
+           
             Console.Write("input your number : ");
-            int input1 = Convert.ToInt32(Console.ReadLine());
-            for (int x = 1; x <= 10; x++)
-            {
-                multiple = x * input1;
-                Console.WriteLine("{0} * {1} = {2}", x, input1, multiple);
-            }
+            MultiplicationTable mt = new();
+            mt.input = Convert.ToInt32(Console.ReadLine());
+            mt.PrintNumbers();
             Console.ReadLine();
-        }  
+        }
+
+        public class MultiplicationTable
+        {
+            public int input;
+            private int multiple;
+
+            public void PrintNumbers()
+            {
+                for (int x = 1; x <= 10; x++)
+                {
+                    multiple = x * input;
+                    Console.WriteLine("{0} * {1} = {2}", x, input, multiple);
+                }
+            }
+        }
+
     }
 }
