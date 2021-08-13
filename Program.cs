fd72ebc25fbdf2a28940941184cbc4ba59ff1837
Fazrin <nirzaf@gmail.com>
2021-08-13 20:11:29 +0530

    Odd Number Updated

--- Program.cs
+++ Program.cs
@@ -8,10 +8,19 @@ namespace app2
         {
             Console.Write("Enter the No.Of Terms : ");
             int input = Convert.ToInt32(Console.ReadLine());
-            for (int x = 1; x <= input; x++)
+            int y = input + input;
+            Console.WriteLine("The Odd Numbers upto {0} are : ", input);
+            int sum = 0;
+            for (int x = 1; x <= y; x++)
             {
-
+               if(x % 2 == 1)
+                {
+                    x += sum;
+                    Console.Write(x + " ");
+                }
+              
             }
-        }  
+            Console.WriteLine("The Sum of Odd Numbers are : {0}", sum);
+        }   
     }
 }
