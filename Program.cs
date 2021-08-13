ec992cd653143b502679910be126d9c138c9e126
Fazrin <nirzaf@gmail.com>
2021-08-13 21:35:32 +0530

    Enter your input:

--- Program.cs
+++ Program.cs
@@ -7,22 +7,47 @@ namespace app2
         static void Main(string[] args)
         {
             Console.Write("Enter the No.Of Terms : ");
-            int input = Convert.ToInt32(Console.ReadLine());
-            int y = input + input;
-            Console.WriteLine("The Odd Numbers upto {0} are : ", input);
-            int sum = 0;
-            for (int x = 1; x <= y; x++)
-            {
-                 if (x % 2 == 1)
-                {
-                   Console.Write(x + " ");
-                    sum += x;
-                }
-                
-                
-            }
-            Console.WriteLine("\nThe Sum of Odd Numbers are : {0}", sum);
+            OddNumbers odd = new OddNumbers();
+            odd.input = Convert.ToInt32(Console.ReadLine());
 
-        }   
+            odd.PrintOddNumbers();
+            //int input = Convert.ToInt32(Console.ReadLine());
+            //int y = input + input;
+            //Console.WriteLine("The Odd Numbers upto {0} are : ", input);
+            //int sum = 0;
+            //for (int x = 1; x <= y; x++)
+            //{
+            //    if (x % 2 == 1)
+            //    {
+            //        Console.Write(x + " ");
+            //        sum += x;
+            //    }            
+            //}
+            //Console.WriteLine("\nThe Sum of Odd Numbers are : {0}", sum);
+
+        }
+
+        public class OddNumbers 
+        { 
+            public int input { get; set; }
+
+            public void PrintOddNumbers()
+            {
+                Console.WriteLine("Enter your input: ");
+
+                int oddNumbers = 1;
+                int sum = 0;
+
+                for (int i = 1; i <= input; i++)
+                {
+                    sum += oddNumbers;
+                    Console.Write(oddNumbers + ", ");
+                    oddNumbers += 2;
+                }
+
+                Console.WriteLine("\n Sum of all odd numbers : {0}" , sum);
+            }
+                    
+        }
     }
 }
