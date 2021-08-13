724f41f22523bbbcbc75abfd9aa8563ce59df899
Fazrin <nirzaf@gmail.com>
2021-08-13 21:37:01 +0530

    Enter your input:

--- Program.cs
+++ Program.cs
@@ -7,23 +7,25 @@ namespace app2
         static void Main(string[] args)
         {
             Console.Write("Enter the No.Of Terms : ");
-            OddNumbers odd = new OddNumbers();
-            odd.input = Convert.ToInt32(Console.ReadLine());
-
-            odd.PrintOddNumbers();
-            //int input = Convert.ToInt32(Console.ReadLine());
-            //int y = input + input;
-            //Console.WriteLine("The Odd Numbers upto {0} are : ", input);
-            //int sum = 0;
-            //for (int x = 1; x <= y; x++)
+            //OddNumbers odd = new()
             //{
-            //    if (x % 2 == 1)
-            //    {
-            //        Console.Write(x + " ");
-            //        sum += x;
-            //    }            
-            //}
-            //Console.WriteLine("\nThe Sum of Odd Numbers are : {0}", sum);
+            //    input = Convert.ToInt32(Console.ReadLine())
+            //};
+            //odd.PrintOddNumbers();
+
+            int input = Convert.ToInt32(Console.ReadLine());
+            int y = input + input;
+            Console.WriteLine("The Odd Numbers upto {0} are : ", input);
+            int sum = 0;
+            for (int x = 1; x <= y; x++)
+            {
+                if (x % 2 == 1)
+                {
+                    Console.Write(x + " ");
+                    sum += x;
+                }
+            }
+            Console.WriteLine("\nThe Sum of Odd Numbers are : {0}", sum);
 
         }
 
