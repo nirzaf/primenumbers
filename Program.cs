d8d3754f62b8c804f3b0a283204e5c104503f0be
Fazrin <nirzaf@gmail.com>
2021-08-13 19:20:52 +0530

    odd updated

--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ namespace app2
     {
         static void Main(string[] args)
         {
-          
+            Console.Write("Enter the No.Of Terms : ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            for (int x = 1; x <= input; x++)
+            {
+
+            }
         }  
     }
 }
