40bc03cad8c94ed7fd3358408b40621fbad6d824
Fazrin <nirzaf@gmail.com>
2021-08-14 18:52:48 +0530

    update

--- Program.cs
+++ Program.cs
@@ -6,20 +6,32 @@ namespace app2
     {
         static void Main(string[] args)
         {
-           
-            Console.Write("Enter the No.Of Term : ");
+
+            /*  Console.Write("Enter the No.Of Term : ");
+              int input = Convert.ToInt32(Console.ReadLine());
+              int square;
+              int sum = 0;
+              Console.Write("The Square Numbers upto {0} Terms ", input + " are :");
+              for ( int x = 1; x <= input; x++)
+              {
+                  square = x * x;
+                  Console.Write(square + " ");
+                  sum += square;
+
+              }
+              Console.WriteLine("\nThe Sum of Square Numbers upto {0}", input +" Terms are :" + sum); */
+
+            Console.Write("Enter the Input : ");
             int input = Convert.ToInt32(Console.ReadLine());
+            Console.Write("The Square Numbers in {0} ", input + " are :");
             int square;
-            int sum = 0;
-            Console.Write("The Square Numbers upto {0} Terms ", input + " are :");
-            for ( int x = 1; x <= input; x++)
+            for (int x = 1; x <= input; x++)
             {
                 square = x * x;
+                Math.Sqrt(square);
                 Console.Write(square + " ");
-                sum += square;
 
             }
-            Console.WriteLine("\nThe Sum of Square Numbers upto {0}", input +" Terms are :" + sum);
         }  
     }
 }
