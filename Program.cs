3fbcef2273f8b616cd43a56be6bd196fef1dd996
Fazrin <nirzaf@gmail.com>
2021-08-14 16:28:47 +0530

    update

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace app2
     {
         static void Main(string[] args)
         {
-            
+            //int number = 0;
+            for(int x = 1; x <= 4; x++)
+            {
+                for(int z = 1; z <= (4-x); z++)
+                {
+                    Console.Write(" ");
+                }
+                for(int y = 1; y <= x; y++)
+                {
+                    // number++;
+                    y++;
+                    Console.Write("*");
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
         }  
     }
 }
