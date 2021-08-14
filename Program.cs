c4e1d506cb6912b6b3877b562a9c447f90333a15
Fazrin <nirzaf@gmail.com>
2021-08-14 15:50:26 +0530

    Right Angle Triangle using Asterisk

--- Program.cs
+++ Program.cs
@@ -7,6 +7,15 @@ namespace app2
         static void Main(string[] args)
         {
             
+            for(int x = 1; x <= 4; x++ )
+            {
+                for(int y = 1; y <= x; y++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
         }  
     }
 }
