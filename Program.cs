0c2e4dabd32ff6b82783184a64e539400e59402c
Fazrin <nirzaf@gmail.com>
2021-08-15 23:06:12 +0530

    //When you want to return multiple values from a method without using ref or out parameters.         //When you want to pass multiple values to a method through a single parameter.         //When you want to hold a database record or some values temporarily without creating a separate class.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,26 @@ namespace app2
     {
         static void Main(string[] args)
         {
-            
+            var t1 = Tuple.Create("Fazrin", 2, 5656, "Nirzaf");
+            Console.WriteLine(t1.Item1);  
+            Console.WriteLine(t1.Item2);  
+            Console.WriteLine(t1.Item3);  
+            Console.WriteLine(t1.Item4);
+
+        //Usage of Tuple
+
+        //When you want to return multiple values from a method without using ref or out parameters.
+        //When you want to pass multiple values to a method through a single parameter.
+        //When you want to hold a database record or some values temporarily without creating a separate class.
+
+
+        //Tuple Limitations:
+
+        //The Tuple is a reference type and not a value type.It allocates on heap and could result in CPU intensive operations.
+        //The Tuple is limited to include eight elements.You need to use nested tuples if you need to store more elements. However, this may result in ambiguity.
+        //The Tuple elements can be accessed using properties with a name pattern Item<elementNumber>, which does not make sense.
+
+
         }  
     }
 }
