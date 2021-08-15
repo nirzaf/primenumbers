0fdcf64d357ec191c21c16c63a3dac7bfc028f8f
Fazrin <nirzaf@gmail.com>
2021-08-15 10:28:31 +0530

    ddd

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace app2
 {
@@ -6,7 +7,24 @@ namespace app2
     {
         static void Main(string[] args)
         {
-            
+            Stack st = new Stack();
+
+            Books b1 = new Books();
+
+            for (int i = 0; i < 50; i++)
+            {
+                b1.BookId = i;
+                b1.BookName = Faker.
+            }
+
         }  
     }
+
+    public class Books 
+    {
+        public int BookId { get; set; }
+        public string BookName { get; set; }
+        public string Author { get; set; }
+    }
+
 }
