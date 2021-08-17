d58ca5bcb26447a835edaeb2ce1c151ee83fa6c9
Fazrin <nirzaf@gmail.com>
2021-08-17 22:07:55 +0530

    Employee

--- Program.cs
+++ Program.cs
@@ -6,7 +6,26 @@ namespace app2
     {
         static void Main(string[] args)
         {
-            
+            var emp = new Employee<string>
+            {
+                Id = "id",
+                FirstName = "Fazrin",
+                LastName = "Farook",
+                Salary = "Monthly"
+            };
+
+            Console.WriteLine("Employee Id : " + emp.Id + ", First Name : " + emp.FirstName + ", Last Name : " + emp.LastName + ", Salary : " + emp.Salary);
+            Console.ReadLine();
         }  
     }
+
+
+    public class Employee<T> where T : class
+    {
+        public T Id { get; set; }
+        public T FirstName { get; set; }
+        public T LastName { get; set; }
+        public T Salary { get; set; }
+    }
+
 }
