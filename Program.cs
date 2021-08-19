ea3e48d952264ef17243ea6a279bd3c8f3ca5b52
Fazrin <nirzaf@gmail.com>
2021-08-19 22:59:08 +0530

    Deduction(out q, out w)

--- Program.cs
+++ Program.cs
@@ -8,12 +8,21 @@ namespace app2
         {
             int x = 10;
             Multiply(ref x);
-            Console.WriteLine("Reference Type Variable : {0}", x);
+            //Console.WriteLine("Reference Type Variable : {0}", x);
+            int q, w;
+            Deduction(out q, out w);
+            Console.WriteLine(x +"  " + w.ToString());
         }
 
         private static void Multiply(ref int a)
         {
             a *= a;
+        }
+
+        private static void Deduction(out int a, out int b)
+        {
+            a = 5;
+            b = 50;
         }
     }
 }
