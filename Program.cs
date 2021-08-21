1083d8ecd545435ecd29e47add978c53ca6eebc9
Mohamed Farook Mohamed Fazrin <mohamed.farzin@idsnext.com>
2021-08-21 22:12:54 +0530

    //assigns year, month, day, hour, min, seconds, UTC timezone             DateTime dt4 = new DateTime(2015, 12, 31, 5, 10, 20, DateTimeKind.Utc);

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace app2
     {
         static void Main(string[] args)
         {
-            
+            //assigns default value 01/01/0001 00:00:00
+            DateTime dt1 = new DateTime(); 
+
+            //assigns year, month, day
+            DateTime dt2 = new DateTime(2015, 12, 31); 
+
+            //assigns year, month, day, hour, min, seconds
+            DateTime dt3 = new DateTime(2015, 12, 31, 5, 10, 20);
+ 
+            //assigns year, month, day, hour, min, seconds, UTC timezone
+            DateTime dt4 = new DateTime(2015, 12, 31, 5, 10, 20, DateTimeKind.Utc);
+
+            Console.WriteLine(dt1);
+            Console.WriteLine(dt2);
+            Console.WriteLine(dt3);
+            Console.WriteLine(dt4);
         }  
     }
 }
