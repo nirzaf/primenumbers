b03989f7cd713de9890b67c8d653a81b3338db5c
Mohamed Farook Mohamed Fazrin <mohamed.farzin@idsnext.com>
2021-08-21 22:29:52 +0530

    Console.WriteLine("Value of b.x = " + b.x);

--- Program.cs
+++ Program.cs
@@ -6,7 +6,23 @@ namespace app2
     {
         static void Main(string[] args)
         {
-            
+              Demo a = new(50, 50);
+              Demo b = a;
+              a.x = 100;
+              Console.WriteLine("Value of a.x = " + a.x);
+              Console.WriteLine("Value of b.x = " + b.x);
         }  
+    }
+
+    //creating structure
+    public struct Demo
+    {
+        public int x, y;
+        //parameterized constructor
+        public Demo(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
     }
 }
