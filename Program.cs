6320ac30289e674993f4850efac2f7b70456621e
Fazrin <nirzaf@gmail.com>
2021-09-05 15:44:39 +0530

    Solo Learn Skee-Ball Exercise Finished

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace app2
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Enter the Points you Scored from Playing : ");
+            int points = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the Cost of the Squirt Gun (in tickets) : ");
+            int squirtGun = Convert.ToInt32(Console.ReadLine());
+            int tickets = points / 12;
+            if(squirtGun <= tickets)
+            {
+                Console.WriteLine("Buy it!");
+            }
+            else
+            {
+                Console.WriteLine("Try again");
+            }
         }  
     }
 }
