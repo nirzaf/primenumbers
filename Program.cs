3d14e7168acfb90ca06b048886bcfab8eb3703ff
Fazrin <nirzaf@gmail.com>
2021-09-05 15:07:31 +0530

    Solo Learn Jungle Camping Exercise

--- Program.cs
+++ Program.cs
@@ -6,6 +6,25 @@ namespace app2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the Animal Sound : ");
+            string animalSound = Console.ReadLine();
+            
+                if (animalSound == "Grr")
+                {
+                    Console.WriteLine("Lion ");
+                }
+                else if (animalSound == "Rawr")
+                {
+                    Console.WriteLine("Tiger ");
+                }
+                else if (animalSound == "Ssss")
+                {
+                    Console.WriteLine("Snake ");
+                }
+                else if (animalSound == "Chirp")
+                {
+                    Console.WriteLine("Bird ");
+                }
             
         }  
     }
