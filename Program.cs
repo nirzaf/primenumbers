72b2f197e3e748b9ab6aa237a2817afd7aef5150
Fazrin <nirzaf@gmail.com>
2021-09-09 17:01:02 +0530

    Argentina Exercise in Solo Learn Completed

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace app2
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Enter the Price in Pesos : ");
+            int pesos = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the Price in Dollars : ");
+            int dollars = Convert.ToInt32(Console.ReadLine());
+            int pesosInDollars = pesos / 50;
+            if( dollars > pesosInDollars)
+            {
+                Console.WriteLine("Pesos");
+            }
+            else if( dollars < pesosInDollars)
+            {
+                Console.WriteLine("Dollars");
+            }
         }  
     }
 }
