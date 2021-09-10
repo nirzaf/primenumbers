989a6c6fcd0acbe7aee12609b39367ff63955a36
nirzaf <mfmfazrin1986@gmail.com>
2021-09-10 14:44:09 +0530

    finished the classes and objects exercise

--- Program.cs
+++ Program.cs
@@ -6,24 +6,28 @@ namespace app2
     {
         static void Main(string[] args)
         {
-            
+            PhotoBooKTest b1 = new PhotoBooKTest();
+
         }  
     }
-    class PhotoBook
+    public class PhotoBook
     {
-        private int numPages;
+        private int numPages{get; set;}
+        public PhotoBook()
+        {
+            numPages = 16;
+        }
+        public PhotoBook(int pages)
+        {
+            numPages = pages;
+        }
         public int GetNumberPages()
         {
             return numPages;
         }
-        public PhotoBook(int pages)
-        {
-            Console.WriteLine(16);
-            pages = numPages;
-        }
      
     }
-    class BigphotoBook
+    public class BigphotoBook
     {
         public BigphotoBook()
         {
@@ -31,8 +35,16 @@ namespace app2
         }
 
     }
-    class PhotoBooKTest
+    public class PhotoBooKTest
     {
-        
+        public PhotoBooKTest()
+        {
+            PhotoBook p1 = new PhotoBook();
+            Console.WriteLine(p1.GetNumberPages());
+            PhotoBook p2 = new PhotoBook (24);
+            Console.WriteLine(p2.GetNumberPages());
+            BigphotoBook p3 = new BigphotoBook();
+        }
+
     }
 }
