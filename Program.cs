ae9464207283b8ece5a56d39052e97ed8563df21
nirzaf <mfmfazrin1986@gmail.com>
2021-09-10 21:36:17 +0530

    strings properties and methods

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace app2
 {
@@ -6,22 +7,19 @@ namespace app2
     {
         static void Main(string[] args)
         {
-            
-
+            string a = "some text";
+            // Console.WriteLine(a.Length); // Length = 9
+            // Console.WriteLine(a.IndexOf('t')); // IndexOf = 5
+            // a = a.Insert(0, "This is ");
+            //Console.WriteLine(a); // This is some text
+            // a = a.Replace("This is ", "I am ");
+            //Console.WriteLine(a); // I am some text
+            //if(a.Contains("some"))
+            // Console.WriteLine("found"); // found
+             //Console.WriteLine(a.Remove(4)); // some
+            Console.WriteLine(a.Substring(2)); // me text
         } 
         
-        public class Fazrin
-        {
-            //Code Added
-            public int Id { get; set; }
-            public string Name { get; set; }
-
-        }  
-
-        public class ZamraBanu
-        {
-            public int MyID { get; set; }
-            public string MyName { get; set; }
-        }
+        
     }
 }
