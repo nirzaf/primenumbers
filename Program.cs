9a262ff853553f22b91e809115c62838bd1b51f2
nirzaf <mfmfazrin1986@gmail.com>
2021-09-11 18:06:04 +0530

    5 < criminals &&

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace app2
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Enter the Total No.Of Criminals Present : ");
+            int criminals = Convert.ToInt32(Console.ReadLine());
+            if (criminals < 5)
+            {
+                Console.WriteLine("I got this!");
+            }
+            else if (criminals <= 10)
+            {
+                Console.WriteLine("Help me Batman");
+            }
+            else
+            {
+                Console.WriteLine("Good Luck out there!");
+            }
 
         } 
         
