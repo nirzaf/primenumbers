7df6f2bc71d8b76e371b17d89aa21249e4f3c864
nirzaf <mfmfazrin1986@gmail.com>
2021-09-11 17:04:42 +0530

    updated

--- Program.cs
+++ Program.cs
@@ -9,27 +9,11 @@ namespace app2
         {
             Console.Write("Enter the Animal Sound : ");
             string animalSound = Console.ReadLine();
-            var data = animalSound.Split(' ');
-            for (int i = 0; i < data.Length; i++)
-            {
-                if (data[i].Contains("Grr"))
-                {
-                    Console.Write("Lion ");
-                }
-                if (data[i].Contains("Rawr"))
-                {
-                    Console.Write("Tiger ");
-                }
-                if (data[i].Contains("Ssss"))
-                {
-                    Console.Write("Snake ");
-                }
-                if (data[i].Contains("Chirp"))
-                {
-                    Console.Write("Bird ");
-                }
-            }
-            
+            animalSound = animalSound.Replace("Grr", "Lion");
+            animalSound = animalSound.Replace("Rawr", "Tiger");
+            animalSound = animalSound.Replace("Ssss", "Snake");
+            animalSound = animalSound.Replace("Chirp", "Bird");
+            Console.WriteLine(animalSound);
         } 
         
     }
