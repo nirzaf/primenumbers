25b08d292d9ee52bf74f0b9a24f8de5d5fb1526b
nirzaf <mfmfazrin1986@gmail.com>
2021-09-11 17:49:24 +0530

    first class and method exercise completed

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
 
 namespace app2
 {
@@ -6,10 +7,28 @@ namespace app2
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter the 3 Names :");
+            Person p = new Person();
+            for(int i =0; i < 3; i++)
+            {
+                string names3 = Console.ReadLine();
+                p.name[i] = names3;
+            }
 
-        } 
-        
-       
+            p.PrintName();
+        }
+
+        class Person
+        {
+            public string[] name = new string[3];
+           
+            public void PrintName()
+            {
+                foreach (var message in name )
+                {
+                    Console.WriteLine("Hello! My name is " + message);
+                }
+            }
+        }
     }
 }
